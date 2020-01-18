931ff35121e7db0ad3072d4b3820d56cc5c09aab
ronak1904 <ronakgoti1904@gmail.com>
2020-01-18 19:40:32 +0530

    source commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:demo_flutter2/service/authentication.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -258,37 +259,50 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         body:new Container(
-          padding: new EdgeInsets.only(left:110,right: 10,top: 140,bottom: 20),
+          
+          padding: new EdgeInsets.only(left:100,right: 10,top: 140,bottom: 20),
           
           child: new Column(
             children: <Widget>[
               new SizedBox(
-                width: 160,
+                width: 180,
               child: new RaisedButton(
-                onPressed: (){},
-                child: new Text('Offer A Ride', style: TextStyle(fontSize:20,height: 2)),
+                onPressed: (){ Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Example()),);},
+                child: new Text('Offer A Ride', style: TextStyle(fontSize:15,height: 2)),
                 
                 color: Colors.blue,
                 
               ),
               ),
               new SizedBox(
-                width: 160,
+                width: 180,
               child: new RaisedButton(
                 onPressed: (){},
-                child: new Text('View A Ride',style: TextStyle(fontSize:20,height:2)),
+                child: new Text('View A Rides Offered',style: TextStyle(fontSize:15,height:2)),
                 color: Colors.blue,
               ),
                
               ),
                new SizedBox(
-                width: 160,
+                width: 180,
                child:new RaisedButton(
                 onPressed: (){},
-                child: new Text('Search A Ride',style: TextStyle(fontSize:20,height: 2)),
+                child: new Text('View Available Ride',style: TextStyle(fontSize:15,height: 2)),
                 color: Colors.blue,
               ),
                ),
+               new SizedBox(
+                width: 185,
+              child: new RaisedButton(
+                onPressed: (){},
+                child: new Text('View Rides Requested', style: TextStyle(fontSize:15,height: 2)),
+                
+                color: Colors.blue,
+                
+              ),
+              ),
             ],
           ),
         ) ,
@@ -313,4 +327,238 @@ class _HomePageState extends State<HomePage> {
           child: Icon(Icons.add),
         ));
   }
+
+ 
 }
+
+class Example extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SecondRoute();
+  //State<StatefulWidget> createState1() => _ExampleState1();
+}
+
+
+
+
+ class _SecondRoute extends State<Example> {
+  var selected;
+   final GlobalKey<FormState> formkey =new GlobalKey<FormState>();
+   List<String> _source=<String>[
+     'Ahmedabad',
+     'Surat',
+     'Bhavnagar',
+     'Gandhinagar',
+
+   ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+    
+        child: new Form(
+         key: formkey,
+         autovalidate: true,
+          child: new ListView(
+            padding: EdgeInsets.only(left: 2.0,right: 2.0),
+            shrinkWrap: true,
+            children: <Widget>[
+               //Text('Source', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+            SizedBox(height: 8.0,width: 40.0,),
+            //padding: EdgeInsets.only(left: 2.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              //mainAxisAlignment: MainAxisAlignment.center,
+              
+              children: <Widget>[
+                Text('Source',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                new Container(
+                height: 48.0,
+                width: 200.0,
+        
+              child:  
+                   DropdownButton(
+                    //labelText:Text('Source', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                   
+                  items:_source.map((value)=>DropdownMenuItem(
+                    
+                    child: Text(
+                      value,
+                      
+                    ),
+                    value: value,
+                  )).toList(),
+                  onChanged: (selectSource)
+                  {
+                    setState(() {
+                      selected=selectSource;   
+                    });
+                  },
+                  value: selected,
+                  isExpanded: false,
+                  hint:Text('Select Source'),
+                  //itemHeight: 10.0,
+                 ),
+                ),
+                 Text('Destination',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,),),
+          new Container(
+                 height: 48.0,
+                 width: 200.0,
+    
+           child:DropdownButton(
+                  
+                  items:_source.map((value)=>DropdownMenuItem(
+                    
+                    child: Text(
+                      value,
+                      
+                    ),
+                    value: value,
+                  )).toList(),
+                  onChanged: (selectSource)
+                  {
+                    setState(() {
+                      selected=selectSource;   
+                    });
+                  },
+                  value: selected,
+                  isExpanded: false,
+                  hint:Text('Select Source'),
+                 
+                  //itemHeight: 10.0,
+                 ),
+              ),
+             RaisedButton(
+               child: Text('choose date and Time'),
+               onPressed: (){
+                 showDatePicker(
+                   context: context,
+                   initialDate: DateTime.now().add(Duration(seconds: 1)),
+                   firstDate: DateTime.now(),
+                   lastDate: DateTime(2100),
+                 );
+               },
+
+              ),
+              ],
+            )
+             
+          
+              
+            ],
+        )),
+      ),
+    );
+  }
+}
+
+
+/*String dropdownValue = 'Ahmedabad';
+class _ExampleState extends State<Example> {
+  Widget build(BuildContext context) {
+    
+  return DropdownButton<String>(
+    value: dropdownValue,
+    icon: Icon(Icons.arrow_downward),
+    iconSize: 24,
+    elevation: 16,
+    style: TextStyle(
+      color: Colors.deepPurple
+    ),
+    underline: Container(
+      height: 2,
+      color: Colors.deepPurpleAccent,
+    ),
+    onChanged: (String newValue) {
+      setState(() {
+              dropdownValue = newValue;
+            });
+          },
+          items: <String>['Ahmedabad', 'Surat', 'Gandhinagar', 'Bhavnagar']
+            .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            })
+            .toList(),
+        );
+  }
+}
+
+String dropdownValue1 = 'Ahmedabad';
+class _ExampleState1 extends State<Example> {
+  Widget build(BuildContext context) {
+    
+  return DropdownButton<String>(
+    value: dropdownValue1,
+    icon: Icon(Icons.arrow_downward),
+    iconSize: 24,
+    elevation: 16,
+    style: TextStyle(
+      color: Colors.deepPurple
+    ),
+    underline: Container(
+      height: 2,
+      color: Colors.deepPurpleAccent,
+    ),
+    onChanged: (String newValue) {
+      setState(() {
+              dropdownValue1 = newValue;
+            });
+          },
+          items: <String>['Ahmedabad', 'Surat', 'Gandhinagar', 'Bhavnagar']
+            .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            })
+            .toList(),
+        );
+  }
+}*/
+
+/*String dropdownValue = 'One';
+Widget showEmailInput(){
+
+  return DropdownButton<String>(
+    value: dropdownValue,
+    icon: Icon(Icons.arrow_downward),
+    iconSize: 24,
+    elevation: 16,
+    style: TextStyle(
+      color: Colors.deepPurple
+    ),
+    underline: Container(
+      height: 2,
+      color: Colors.deepPurpleAccent,
+    ),
+    onChanged: (String newValue) {
+      setState(() {
+              dropdownValue = newValue;
+            });
+          },
+          items: <String>['One', 'Two', 'Free', 'Four']
+            .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            })
+            .toList(),
+        );
+      }
+
+      */
+
+   // @protected
+
+//void setState(() { _myState = newValue });
+
+      
+      
