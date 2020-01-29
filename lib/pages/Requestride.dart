e8400fa7a72916ff05c895d3014b8f9f1ca2a864
ronak1904 <ronakgoti1904@gmail.com>
2020-01-30 00:03:14 +0530

    Start Requesting commit

---
+++ lib/pages/Requestride.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:demo_flutter2/pages/Search.dart';
+//import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+//import 'package:demo_flutter2/service/authentication.dart';
+//import 'package:firebase_database/firebase_database.dart';
+//import 'package:demo_flutter2/models/todo.dart';
+import 'dart:async';
+//import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+
+
+
+class Requestride extends StatelessWidget {
+
+ Requestride({this.v6});
+  int selectRadio;
+ final String v6;
+ int g=1;
+  TextEditingController _tx1 = new TextEditingController();
+   Widget build(BuildContext context) {
+     
+     return Scaffold(
+      appBar: AppBar(
+        title: Text("Request Ride"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+            //key: formkey,
+            autovalidate: true,
+            child: new ListView(
+              padding: EdgeInsets.only(left: 2.0, right: 2.0),
+              shrinkWrap: true,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                  
+                    new Container(
+                      height: 60.0,
+                      child: TextField(
+                        controller: _tx1,
+                        maxLength: 100,
+                        decoration: InputDecoration(
+                          labelText: " Name",
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            borderSide: new BorderSide(),
+                          ),
+                        ),
+                      ),
+                    ),
+                      
+                    new Container(
+                      height: 60.0,
+                      child: TextField(
+                        controller: _tx1,
+                        maxLength: 100,
+                        decoration: InputDecoration(
+                          labelText: "Age",
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            borderSide: new BorderSide(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Radio(
+                      value:"male",
+                      groupValue: g,
+                      
+
+                    ),
+                    new Padding(
+                        padding: EdgeInsets.only(top: 30.0, bottom: 0.0)),
+                    new Container(
+                      width: 150.0,
+                      height: 50.0,
+                      //new Padding(padding: EdgeInsets.only(top: 20.0)),
+                      // margin: EdgeInsets.only(top:0.0),
+                      //padding: EdgeInsets.only(top: 0.0),
+                      child: RaisedButton(
+                          onPressed: () {
+                            
+                            //print('ret data is $retData');
+                          },
+                          child: new Text('Done',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  height: 2,
+                                  fontWeight: FontWeight.bold)),
+                          color: Colors.blue,
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20.0))),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
